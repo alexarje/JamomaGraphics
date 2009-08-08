96ecc1615bbc8034de7a3b5f58214bbe4b07380f
Timothy Place <tim@electrotap.com>
2009-08-08 12:31:50 -0500

    work to get the SpatDisplay extension compiling again

---
+++ library/includes/TTGraphics.h
@@ -0,0 +1,18 @@
+/* 
+ * JamomaGraphics API
+ * Copyright © 2009 by Timothy Place
+ * This object implements a light wrapper around Cairo
+ * 
+ * License: This code is licensed under the terms of the GNU LGPL
+ * http://www.gnu.org/licenses/lgpl.html 
+ */
+
+#ifndef __TTGRAPHICS_H__
+#define __TTGRAPHICS_H__
+
+#include "TTFoundationAPI.h"
+#include "TTGraphicsContext.h"
+
+void TTGraphicsInit();
+
+#endif // __TTGRAPHICS_H__
